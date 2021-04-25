6664e13492fef9686da0ed5d51e6e551dbd53209
AniqueKhan <aniquekhan004@gmail.com>
2021-04-26 04:30:59 +0500

    final

---
+++ pong.py
@@ -0,0 +1,158 @@
+# Ping Pong Game 2D
+
+
+import turtle
+import time
+
+wn = turtle.Screen()
+wn.title("Ping Pong Game")
+wn.bgcolor("black")
+wn.tracer(0)
+wn.setup(width=800,height=600)
+
+
+# Score
+
+score_a=0
+score_b=0
+
+
+# Paddle A 
+
+paddle_a = turtle.Turtle()
+paddle_a.shape("square")
+paddle_a.speed(0)
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5,stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350,0)
+
+
+
+# Paddle B 
+
+paddle_b = turtle.Turtle()
+paddle_b.shape("square")
+paddle_b.speed(0)
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5,stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350,0)
+
+# Ball
+
+
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0,0)
+
+# Controlling the speed of the ball
+ball.dx = 3
+ball.dy = 3
+
+
+# Pen
+
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0,260)
+pen.write("Player A : 0 , Player B : 0", align="center",font=("Courier",18,"normal"))
+
+
+
+
+
+# Functions
+
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y +=20
+    paddle_a.sety(y)
+
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -=20
+    paddle_a.sety(y)
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y +=20
+    paddle_b.sety(y)
+
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -=20
+    paddle_b.sety(y)
+
+
+
+# Keyboard Bindings
+
+wn.listen()
+wn.onkeypress(paddle_a_up,"w")
+wn.onkeypress(paddle_a_down,"s")
+wn.onkeypress(paddle_b_up,"Up")
+wn.onkeypress(paddle_b_down,"Down")
+
+
+
+# Main Game Loop
+while True:
+    wn.update()
+
+    # Moving The Ball
+
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # Border Checking
+
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *=-1
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *=-1
+
+    if ball.xcor() > 390:
+        ball.goto(0,0)
+        ball.dx *=-1
+        score_a +=1
+        pen.clear()
+        pen.write("Player A : {} , Player B : {}".format(score_a,score_b) , align="center",font=("Courier",18,"normal"))
+
+    if ball.xcor() < -390:
+        ball.goto(0,0)
+        ball.dx *=-1
+        score_b +=1
+        pen.clear()
+        pen.write("Player A : {} , Player B : {}".format(score_a,score_b) , align="center",font=("Courier",18,"normal"))
+
+
+    # Paddle And Ball Collisions
+
+    if (ball.xcor() > 335 and ball.xcor() < 350) and (ball.ycor() > paddle_b.ycor() -50 and ball.ycor() < paddle_b.ycor() +50):
+        ball.setx(335)
+        ball.dx *=-1
+
+    if (ball.xcor() < -335 and ball.xcor() > -350) and (ball.ycor() > paddle_a.ycor() -50 and ball.ycor() < paddle_a.ycor() +50):
+        ball.setx(-335)
+        ball.dx *=-1
+
+
+    # There's an unusual bug in the code , the speed of the ball keeps increasing and decreasing randomnly
+    # For that purpose we use time.sleep()
+
+    
+    time.sleep(1/120)
+
+print("HHHHH")
